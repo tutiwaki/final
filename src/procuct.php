e621e52be92de3617b21873c5a3a476a72440242
tutiwaki <148731278+tutiwaki@users.noreply.github.com>
2024-01-12 12:29:22 +0900

    i

--- src/procuct.php
+++ src/procuct.php
@@ -5,7 +5,7 @@
 <hr>
 <?php
 echo '<table>';
-echo '<tr><th>商品番号</th><th>商品名</th></tr>';
+echo '<tr><th>商品番号</th><th>商品名</th><th><th></tr>';
 $pdo = new PDO('mysql:host=mysql218.phy.lolipop.lan;dbname=LAA1516969-aso2201217', 'LAA1516969', 'Narimo1930');
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['keyword'])) {
     $keyword = '%' . $_POST['keyword'] . '%';
@@ -25,9 +25,9 @@ foreach ($sql as $row) {
     echo '<td>', $id, '</td>';
     echo '<td>';
     echo '<a href=detail.php?id=', $id, '">', $row['name'], '</a>';
-    echo '</td>';
+    echo '</td>',"<input type='button' onClick= 'document.location.href='favorite-delete.php''value=削除'/><br>";
     echo '</tr>';
-    "<input type='button' onClick= 'document.location.href='favorite-delete.php''value=削除'/><br>";
+    
 }
 echo '</table>';
 ?>
