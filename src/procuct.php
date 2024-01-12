a75b5f20331a9345ce3df8b30edfb8b67f27d0e1
tutiwaki <148731278+tutiwaki@users.noreply.github.com>
2024-01-12 12:25:59 +0900

    h

--- src/procuct.php
+++ src/procuct.php
@@ -27,7 +27,6 @@ foreach ($sql as $row) {
     echo '<a href=detail.php?id=', $id, '">', $row['name'], '</a>';
     echo '</td>';
     echo '</tr>';
-    echo $sql['koku_name'],'：', $row['sensyu_name'],
     "<input type='button' onClick= 'document.location.href='favorite-delete.php''value=削除'/><br>";
 }
 echo '</table>';
