ac33543c3b466e417526f7a2cdb56ca86b3ba79e
tutiwaki <148731278+tutiwaki@users.noreply.github.com>
2024-01-12 10:50:07 +0900

    Update procuct.php

--- src/procuct.php
+++ src/procuct.php
@@ -1,26 +1,23 @@
 <?php require './header.php'; ?>
 <?php require 'menu.php'; ?>
 <form action="procuct.php" method="post">
-    商品検索
-    <input type="text" name="keyword">
-    <input type="submit" value="検索">
 </form>
 <hr>
 <?php
 echo '<table>';
-echo '<tr><th>商品番号</th><th>商品名</th><th>価格</th><th>';
-$pdo = new PDO('mysql:host=mysql214.phy.lolipop.lan;dbname=LAA1516969-shop', 'LAA1516969', 'Pass1112');
+echo '<tr><th>商品番号</th><th>商品名</th></tr>';
+$pdo = new PDO('mysql:host=mysql218.phy.lolipop.lan;dbname=LAA1516969-aso2201217', 'LAA1516969', 'Narimo1930');
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['keyword'])) {
     $keyword = '%' . $_POST['keyword'] . '%';
-    $sql = $pdo->prepare('select * from product where name like ? ');
+    $sql = $pdo->prepare('select * from Syoutyu where name like ? ');
     $sql->execute(['%'.$_POST['keyword'].'%']);
 } elseif ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET['id'])) {
     // Assuming you want to handle GET requests with an 'id' parameter
     $id = $_GET['id'];
-    $sql = $pdo->prepare('select * from product where id = ?');
+    $sql = $pdo->prepare('select * from Syoutyu where id = ?');
     $sql->execute([$id]);
 } else {
-    $sql = $pdo->query('select * from product');
+    $sql = $pdo->query('select * from Syoutyu');
 }
 foreach ($sql as $row) {
     $id = $row['id'];
@@ -29,7 +26,6 @@ foreach ($sql as $row) {
     echo '<td>';
     echo '<a href=detail.php?id=', $id, '">', $row['name'], '</a>';
     echo '</td>';
-    echo '<td>', $row['price'], '</td>';
     echo '</tr>';
 }
 echo '</table>';
