15065d999effa04dcd062076ea21bf3b05bf86ad
tutiwaki <148731278+tutiwaki@users.noreply.github.com>
2024-01-12 10:54:22 +0900

    jiu

--- src/menu.php
+++ src/menu.php
@@ -1,9 +1,5 @@
 <a href="procuct.php">商品</a>
-<a href="favorite-show.php">お気に入り</a>
-<a href="history.php">購入履歴</a>
-<a href="cart-show.php">カート</a>
-<a href="purchase-input.php">購入</a>
-<a href="login-input.php">ログイン</a>
-<a href="loguauto-input.php">ログアウト</a>
-<a href="customer-input.php">会員登録</a>
+<a href="favorite-show.php">更新</a>
+<a href="history.php">削除</a>
+<a href="cart-show.php">新規登録</a>
 <hr>
