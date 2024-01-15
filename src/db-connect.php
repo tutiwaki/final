cc94fcd97ce50efcbb279d2ac7edf618c6a09cb1
tutiwaki <148731278+tutiwaki@users.noreply.github.com>
2024-01-15 09:51:37 +0900

    u

--- src/db-connect.php
+++ src/db-connect.php
@@ -1,8 +1,8 @@
 <?php
     const SERVER = 'mysql214.phy.lolipop.lan';
-    const DBNAME = 'LAA1516969-shop';
+    const DBNAME = 'LAA1516969-aso2201217';
     const USER = 'LAA1516969';
-    const PASS = 'Pass1112';
+    const PASS = 'Narimo1930';
 
     $connect = 'mysql:host='. SERVER . ';dbname='. DBNAME . ';charset=utf8';
 ?>
